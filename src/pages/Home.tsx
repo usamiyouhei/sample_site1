7c41fd12d4faa162f8a962fa745405a4463400ce
youhei usami <usao9110@icloud.com>
2026-01-14 13:32:03 +0400

    1/14

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Home() {
-  return <div></div>;
-}
+const Home = () => {
+  return (
+    <div>
+      <main>Sample Site</main>
+    </div>
+  );
+};
 
 export default Home;
