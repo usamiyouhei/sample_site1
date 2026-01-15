e1cf0b4a2da37ca8b860fa9efeb9f1cf931d31a3
youhei usami <usao9110@icloud.com>
2026-01-15 11:14:55 +0400

    1/15

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header className="header">
+        <div className="header__inner">
+          <h1 className="header__logo">System Kitchen</h1>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
